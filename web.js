45fc534a454963ebabdfe58ebeca2be0a2fdd130
ShaunCode1 <shauncode1@gmail.com>
2013-07-10 04:27:14 +0000

    Read web message from a file

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// response.send('Hello World2!');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send( fs.readFileSync('index.html').toString() );
 });
 
 var port = process.env.PORT || 5000;
